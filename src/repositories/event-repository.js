9ecbecf337952f022b6da1f446070933dd2138aa
Android Studio <devandroid@ort>
2024-07-05 11:21:12 -0300

    12 terminado

--- src/repositories/event-repository.js
+++ src/repositories/event-repository.js
@@ -1,73 +1,68 @@
-import pkg from 'pg';
+import pkg from "pg";
 const { Client } = pkg;
-import config from '../configs/db-config.js';
+import config from "../configs/db-config.js";
 
 export default class EventRepository {
+  // async getAllAsync() {
+  //     const client = new Client(config);
+  //     await client.connect();
 
-    // async getAllAsync() {
-    //     const client = new Client(config);
-    //     await client.connect();
+  //     try {
+  //         const sql = `SELECT * FROM events`;
+  //         const result = await client.query(sql);
+  //         return result.rows;
+  //     } finally {
+  //         await client.end();
+  //     }
+  // }
 
-    //     try {
-    //         const sql = `SELECT * FROM events`;
-    //         const result = await client.query(sql);
-    //         return result.rows;
-    //     } finally {
-    //         await client.end();
-    //     }
-    // }
+  searchAsync = async (params) => {
+    const client = new Client(config);
+    await client.connect();
 
-    
-    searchAsync = async (params) => {
-        const client = new Client(config);
-        await client.connect();
+    try {
+      let sql = `SELECT e.* FROM events as e INNER JOIN event_categories as ec ON e.id_event_category = ec.id INNER JOIN event_tags et ON e.id = et.id_event INNER JOIN tags as t on t.id = et.id_tag`;
+      const values = [];
+      const conditions = [];
 
-        try {
-            let sql = `SELECT e.* FROM events as e INNER JOIN event_categories as ec ON e.id_event_category = ec.id INNER JOIN event_tags et ON e.id = et.id_event INNER JOIN tags as t on t.id = et.id_tag`;
-            const values = [];
-            const conditions = [];
+      if (params.name) {
+        conditions.push(`e.name ILIKE $${values.length + 1}`);
+        values.push(`%${params.name}%`);
+      }
 
-            if (params.name) {
-                conditions.push(`e.name ILIKE $${values.length + 1}`);
-                values.push(`%${params.name}%`);
-            }
-
-            if (params.category) {
-                conditions.push(`ec.name ILIKE $${values.length + 1} 
+      if (params.category) {
+        conditions.push(`ec.name ILIKE $${values.length + 1} 
                 `);
-                values.push(`%${params.category}%`);
-            }   
+        values.push(`%${params.category}%`);
+      }
 
-            if (params.startdate) {
-                conditions.push(`e.start_date = $${values.length + 1}`);
-                values.push(params.startdate);
-                
-            }
+      if (params.startdate) {
+        conditions.push(`e.start_date = $${values.length + 1}`);
+        values.push(params.startdate);
+      }
 
-            if (params.tag) { 
-                conditions.push(`t.name ILIKE $${values.length + 1}  `);
-                values.push(`%${params.tag}%`);
-            }
+      if (params.tag) {
+        conditions.push(`t.name ILIKE $${values.length + 1}  `);
+        values.push(`%${params.tag}%`);
+      }
 
-            if (conditions.length > 0) {
-                sql += ` WHERE ${conditions.join(' AND ')}`;
-            }
+      if (conditions.length > 0) {
+        sql += ` WHERE ${conditions.join(" AND ")}`;
+      }
 
-            const result = await client.query(sql, values);
-            return result.rows;
-        } finally {
-            await client.end();
-        }
-    
+      const result = await client.query(sql, values);
+      return result.rows;
+    } finally {
+      await client.end();
     }
+  };
 
+  getByIdAsync = async (id) => {
+    const client = new Client(config);
+    await client.connect();
 
-    getByIdAsync = async (id) => {
-        const client = new Client(config);
-        await client.connect();
-    
-        try {
-            let sql = `
+    try {
+      let sql = `
                 SELECT 
                     e.id, e.name, e.description, e.id_event_category, 
                     e.id_event_location, e.start_date, e.duration_in_minutes, 
@@ -98,74 +93,73 @@ export default class EventRepository {
                     event_categories AS ec ON e.id_event_category = ec.id
                 WHERE 
                     e.id = $1`;
-    
-            const values = [id];
-            const result = await client.query(sql, values);
-            const eventData = result.rows[0]; 
-    
-            if (!eventData) {
-                return null;
-            }
-    
-            const event = {
-                id: eventData.id,
-                name: eventData.name,
-                description: eventData.description,
-                id_event_category: eventData.id_event_category,
-                id_event_location: eventData.id_event_location,
-                start_date: eventData.start_date,
-                duration_in_minutes: eventData.duration_in_minutes,
-                price: eventData.price,
-                enabled_for_enrollment: eventData.enabled_for_enrollment,
-                max_assistance: eventData.max_assistance,
-                creator_user: {
-                    id: eventData.creator_user_id,
-                    first_name: eventData.creator_first_name,
-                    last_name: eventData.creator_last_name,
-                    username: eventData.creator_username,
-                    password: eventData.creator_password
-                },
-                event_location: {
-                    id: eventData.event_location_id,
-                    id_location: eventData.id_location,
-                    name: eventData.location_name,
-                    full_address: eventData.full_address,
-                    max_capacity: eventData.max_capacity,
-                    latitude: eventData.location_latitude,
-                    longitude: eventData.location_longitude,
-                    location: {
-                        id: eventData.location_id,
-                        name: eventData.location_name,
-                        id_province: eventData.id_province,
-                        latitude: eventData.province_latitude,
-                        longitude: eventData.province_longitude,
-                        province: {
-                            id: eventData.province_id,
-                            name: eventData.province_name,
-                            full_name: eventData.province_full_name
-                        }
-                    }
-                },
-                event_category: {
-                    id: eventData.event_category_id,
-                    name: eventData.event_category_name,
-                    display_order: eventData.event_category_display_order
-                }
-            };
-    
-            return event;
-        } finally {
-            await client.end();
-        }
-    };
 
+      const values = [id];
+      const result = await client.query(sql, values);
+      const eventData = result.rows[0];
 
-    searchEnrollments = async (eventId, params) => {
-        const client = new Client(config);
-        await client.connect();
-    
-        try {
-            let sql = `
+      if (!eventData) {
+        return null;
+      }
+
+      const event = {
+        id: eventData.id,
+        name: eventData.name,
+        description: eventData.description,
+        id_event_category: eventData.id_event_category,
+        id_event_location: eventData.id_event_location,
+        start_date: eventData.start_date,
+        duration_in_minutes: eventData.duration_in_minutes,
+        price: eventData.price,
+        enabled_for_enrollment: eventData.enabled_for_enrollment,
+        max_assistance: eventData.max_assistance,
+        creator_user: {
+          id: eventData.creator_user_id,
+          first_name: eventData.creator_first_name,
+          last_name: eventData.creator_last_name,
+          username: eventData.creator_username,
+          password: eventData.creator_password,
+        },
+        event_location: {
+          id: eventData.event_location_id,
+          id_location: eventData.id_location,
+          name: eventData.location_name,
+          full_address: eventData.full_address,
+          max_capacity: eventData.max_capacity,
+          latitude: eventData.location_latitude,
+          longitude: eventData.location_longitude,
+          location: {
+            id: eventData.location_id,
+            name: eventData.location_name,
+            id_province: eventData.id_province,
+            latitude: eventData.province_latitude,
+            longitude: eventData.province_longitude,
+            province: {
+              id: eventData.province_id,
+              name: eventData.province_name,
+              full_name: eventData.province_full_name,
+            },
+          },
+        },
+        event_category: {
+          id: eventData.event_category_id,
+          name: eventData.event_category_name,
+          display_order: eventData.event_category_display_order,
+        },
+      };
+
+      return event;
+    } finally {
+      await client.end();
+    }
+  };
+
+  searchEnrollments = async (eventId, params) => {
+    const client = new Client(config);
+    await client.connect();
+
+    try {
+      let sql = `
                 SELECT 
                     e.id, e.id_event, e.id_user, e.description, 
                     e.registration_date_time, e.attended, e.observations, e.rating,
@@ -178,84 +172,79 @@ export default class EventRepository {
                     events AS ev ON e.id_event = ev.id
                 WHERE 
                     ev.id = $1`;
-    
-            const values = [eventId];
-    
-            if (params != null) {
-                if (params.first_name !== undefined) {
-                    sql += ` AND u.first_name ILIKE $${values.length + 1}`;
-                    values.push(`%${params.first_name}%`);
-                }
-    
-                if (params.last_name !== undefined) {
-                    sql += ` AND u.last_name ILIKE $${values.length + 1}`;
-                    values.push(`%${params.last_name}%`);
-                }
-    
-                if (params.username !== undefined) {
-                    sql += ` AND u.username ILIKE $${values.length + 1}`;
-                    values.push(`%${params.username}%`);
-                }
-    
-                if (params.attended !== undefined) {
-                    sql += ` AND e.attended = $${values.length + 1}`;
-                    values.push(params.attended);
-                }
-    
-                if (params.rating !== undefined) {
-                    sql += ` AND e.rating >= $${values.length + 1}`;
-                    values.push(params.rating);
-                }
-            }
-    
-            const result = await client.query(sql, values);
-            const enrollments = result.rows;
-    
-            const collection = enrollments.map(enrollment => ({
-                id: enrollment.id,
-                id_event: enrollment.id_event,
-                id_user: enrollment.id_user,
-                user: {
-                    id: enrollment.user_id,
-                    first_name: enrollment.first_name,
-                    last_name: enrollment.last_name,
-                    username: enrollment.username,
-                    password: enrollment.password
-                },
-                description: enrollment.description,
-                registration_date_time: enrollment.registration_date_time,
-                attended: enrollment.attended,
-                observations: enrollment.observations,
-                rating: enrollment.rating
-            }));
-    
-            const pagination = {
-                limit: 0,
-                offset: 0,
-                nextPage: "",
-                total: collection.length
-            };
-    
-            return { collection, pagination };
-        } finally {
-            await client.end();
+
+      const values = [eventId];
+
+      if (params != null) {
+        if (params.first_name !== undefined) {
+          sql += ` AND u.first_name ILIKE $${values.length + 1}`;
+          values.push(`%${params.first_name}%`);
         }
-    };
-    
 
+        if (params.last_name !== undefined) {
+          sql += ` AND u.last_name ILIKE $${values.length + 1}`;
+          values.push(`%${params.last_name}%`);
+        }
 
-    //     const client = new Client(config);
-    //     await client.connect();
+        if (params.username !== undefined) {
+          sql += ` AND u.username ILIKE $${values.length + 1}`;
+          values.push(`%${params.username}%`);
+        }
 
-    //     try {
-    //         const sql = `SELECT * FROM events`;
-    //         const result = await client.query(sql);
-    //         return result.rows;
-    //     } finally {
-    //         await client.end();
-    //     }
-    // }
+        if (params.attended !== undefined) {
+          sql += ` AND e.attended = $${values.length + 1}`;
+          values.push(params.attended);
+        }
 
-    
+        if (params.rating !== undefined) {
+          sql += ` AND e.rating >= $${values.length + 1}`;
+          values.push(params.rating);
+        }
+      }
 
+      const result = await client.query(sql, values);
+      const enrollments = result.rows;
+
+      const collection = enrollments.map((enrollment) => ({
+        id: enrollment.id,
+        id_event: enrollment.id_event,
+        id_user: enrollment.id_user,
+        user: {
+          id: enrollment.user_id,
+          first_name: enrollment.first_name,
+          last_name: enrollment.last_name,
+          username: enrollment.username,
+          password: enrollment.password,
+        },
+        description: enrollment.description,
+        registration_date_time: enrollment.registration_date_time,
+        attended: enrollment.attended,
+        observations: enrollment.observations,
+        rating: enrollment.rating,
+      }));
+
+      const pagination = {
+        limit: 0,
+        offset: 0,
+        nextPage: "",
+        total: collection.length,
+      };
+
+      return { collection, pagination };
+    } finally {
+      await client.end();
+    }
+  };
+
+  //     const client = new Client(config);
+  //     await client.connect();
+
+  //     try {
+  //         const sql = `SELECT * FROM events`;
+  //         const result = await client.query(sql);
+  //         return result.rows;
+  //     } finally {
+  //         await client.end();
+  //     }
+  // }
 }
--- src/repositories/province-repository.js
+++ src/repositories/province-repository.js
@@ -1,80 +1,83 @@
-import pkg from 'pg'
-import Province from '../entities/province.js';
+import pkg from "pg";
+import Province from "../entities/province.js";
 
-import config from '../configs/db-config.js';
-const { Client } = pkg
+import config from "../configs/db-config.js";
+const { Client } = pkg;
 const client = new Client(config);
 
 await client.connect();
 export default class ProvinceRepository {
-    
-    getAllAsync = async () => {
-       
-        let sql = `SELECT * from provinces`;
-        let result = await client.query(sql)
-        const provincias = result.rows;
-        return provincias
-    }
-    getByIdAsync = async (id) => {
-        let sql = `SELECT * from provinces WHERE id=$1`;
-        const values = [id];
-        let result = await client.query(sql, values)
-        const provincia = result.rows;
-        return provincia
-    }
-    createAsync = async (body) => {
-        try{
-        let nombre = body.name;
-        let full_nombre = body.full_name;
-        let latitud = parseInt(body.latitude);
-        let longitud = parseInt(body.longitude);
-        let display_orden = parseInt(body.display_order);
-        const sql = `
+  getAllAsync = async () => {
+    let sql = `SELECT * from provinces`;
+    let result = await client.query(sql);
+    const provincias = result.rows;
+    return provincias;
+  };
+  getByIdAsync = async (id) => {
+    let sql = `SELECT * from provinces WHERE id=$1`;
+    const values = [id];
+    let result = await client.query(sql, values);
+    const provincia = result.rows;
+    return provincia;
+  };
+  createAsync = async (body) => {
+    try {
+      let nombre = body.name;
+      let full_nombre = body.full_name;
+      let latitud = parseInt(body.latitude);
+      let longitud = parseInt(body.longitude);
+      let display_orden = parseInt(body.display_order);
+      const sql = `
             INSERT INTO provinces
                 (name, full_name, latitude, longitude, display_order)
             VALUES
                 ($1,$2,$3,$4,$5)`;
-        const values = [nombre, full_nombre, latitud, longitud,display_orden];
-        const result = await client.query(sql, values);
-        return ["created",200]; 
-        }
-        catch (error){
-            return [error,404];
-        }
+      const values = [nombre, full_nombre, latitud, longitud, display_orden];
+      const result = await client.query(sql, values);
+      return ["created", 200];
+    } catch (error) {
+      return [error, 404];
     }
-    putAsync = async (body) => {
-        try {
-            const sql1 = `SELECT id from provinces WHERE id=$1`;
-            const valuesID = [parseInt(body.id)];
-            const resultID = await client.query(sql1, valuesID);
-    
-            if (resultID.rows.length === 0) {
-                return ["Provincia no encontrada", 404];
-            }
-    
-            const sql2 = `UPDATE provinces
+  };
+  putAsync = async (body) => {
+    try {
+      const sql1 = `SELECT id from provinces WHERE id=$1`;
+      const valuesID = [parseInt(body.id)];
+      const resultID = await client.query(sql1, valuesID);
+
+      if (resultID.rows.length === 0) {
+        return ["Provincia no encontrada", 404];
+      }
+
+      const sql2 = `UPDATE provinces
                           SET name = $1,
                               full_name = $2,
                               latitude = $3,
                               longitude = $4,
                               display_order = $5
                           WHERE id = $6`;
-    
-            const values = [body.name, body.full_name, body.latitude, body.longitude, body.display_order, body.id];
-            const result = await client.query(sql2, values);
-    
-            if (body.name === "" || body.name.length <= 3) {
-                return ["No cumple con las reglas de negocio", 400];
-            }
-    
-            return ["Update", 200];
-        } catch (error) {
-            return [error.message, 400];
-        }
+
+      const values = [
+        body.name,
+        body.full_name,
+        body.latitude,
+        body.longitude,
+        body.display_order,
+        body.id,
+      ];
+      const result = await client.query(sql2, values);
+
+      if (body.name === "" || body.name.length <= 3) {
+        return ["No cumple con las reglas de negocio", 400];
+      }
+
+      return ["Update", 200];
+    } catch (error) {
+      return [error.message, 400];
     }
-    
-        
-        /*let resArray = "";
+  };
+
+  /*let resArray = "";
         const createPutAsync = body.id;
         const provincefv = provincias.findIndex(provincia => provincia.id == body.id)
         if (provincefv != -1) {
@@ -100,56 +103,60 @@ export default class ProvinceRepository {
             resArray = ["Provincia no encontrada",404]
         }
         return resArray;*/
-        putAsync = async (body) => {
-            try {
-                const sql1 = `SELECT id from provinces WHERE id=$1`;
-                const valuesID = [parseInt(body.id)];
-                const resultID = await client.query(sql1, valuesID);
-        
-                if (resultID.rows.length === 0) {
-                    return ["Provincia no encontrada", 404];
-                }
-        
-                const sql2 = `UPDATE provinces
+  putAsync = async (body) => {
+    try {
+      const sql1 = `SELECT id from provinces WHERE id=$1`;
+      const valuesID = [parseInt(body.id)];
+      const resultID = await client.query(sql1, valuesID);
+
+      if (resultID.rows.length === 0) {
+        return ["Provincia no encontrada", 404];
+      }
+
+      const sql2 = `UPDATE provinces
                               SET name = $1,
                                   full_name = $2,
                                   latitude = $3,
                                   longitude = $4,
                                   display_order = $5
                               WHERE id = $6`;
-        
-                const values = [body.name, body.full_name, body.latitude, body.longitude, body.display_order, body.id];
-                const result = await client.query(sql2, values);
-        
-                if (body.name === "" || body.name.length <= 3) {
-                    return ["No cumple con las reglas de negocio", 400];
-                }
-        
-                return ["Update", 200];
-            } catch (error) {
-                return [error.message, 400];
-            }
-        }
-        
-   
-    deleateAsync = async (id) => {
-        let resArray;
-        const valuesID = [parseInt(id)];
-        try
-        {
-            let sql2 = `DELETE FROM provinces WHERE id=$1`
-            let result = await client.query(sql2, valuesID)
-            resArray = ["Provincia eliminada correctamente",200];
-        }
-        catch(error)
-        {
-            resArray = ["Provincia no encontrada",404];
-            console.log(error);
-        }
-        
-        return resArray;
-    } }
-    /*
+
+      const values = [
+        body.name,
+        body.full_name,
+        body.latitude,
+        body.longitude,
+        body.display_order,
+        body.id,
+      ];
+      const result = await client.query(sql2, values);
+
+      if (body.name === "" || body.name.length <= 3) {
+        return ["No cumple con las reglas de negocio", 400];
+      }
+
+      return ["Update", 200];
+    } catch (error) {
+      return [error.message, 400];
+    }
+  };
+
+  deleateAsync = async (id) => {
+    let resArray;
+    const valuesID = [parseInt(id)];
+    try {
+      let sql2 = `DELETE FROM provinces WHERE id=$1`;
+      let result = await client.query(sql2, valuesID);
+      resArray = ["Provincia eliminada correctamente", 200];
+    } catch (error) {
+      resArray = ["Provincia no encontrada", 404];
+      console.log(error);
+    }
+
+    return resArray;
+  };
+}
+/*
     getByIdAsync = async (id) => {...}
     createAsync = async (entity) => {...}
     updateAsync = async (entity) => {...}
