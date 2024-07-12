0eae4e7fab24ee473fc6f35f099d50ac7a33ffca
Android Studio <devandroid@ort>
2024-07-12 08:24:46 -0300

    avanzando

--- src/repositories/event-location-repository.js
+++ src/repositories/event-location-repository.js
@@ -32,12 +32,16 @@ export default class EventLocationRepository {
     }
     getByIdAsync = async (id) => {
         
-        payloadOriginal = await jwt.verify(validacionToken,secretKey);
+        try{
+            const secretKey = "ClaveSecreta3000$";
+        let payloadOriginal = null;
+        payloadOriginal = await jwt.verify(token,secretKey);
         if(payloadOriginal != null){
         let sql = `SELECT el.id, l.* , el.name, el.full_address, el.max_capacity, el.latitude, el.longitude, u.*
         FROM public.event_locations el inner join locations l on l.id = el.id_location inner join users u on u.id = el.id_creator_user WHERE el.id=$1`;
         const values = [id];
         let result = await client.query(sql, values);
+        console.log(result);
         let sql1 = 'select id_creator_user from event_locations where id = $1'
         let result1 = await client.query(sql1, values);
         console.log(payloadOriginal);
@@ -47,6 +51,32 @@ export default class EventLocationRepository {
         }else{
             return ["not found",404];
         }
+        }else{
+            return ["not found",404];
         }
+        }catch(e){
+
+        }
+        
     }
+    createAsync = async (body) => {
+        try {
+          const sql1 = `SELECT id FROM public.users ORDER BY id DESC limit 1;`;
+          const result1 = await client.query(sql1);
+          let obj = result1.rows[0];
+          const id = obj.id + 1;
+          let nombre = body.name;
+          let display_order = body.display_order;
+          const sql = `
+                INSERT INTO event_categories
+                    (id,name,display_order)
+                VALUES
+                    ($1,$2,$3)`;
+          const values = [id, nombre, parseInt(display_order)];
+          const result = await client.query(sql, values);
+          return ["created", 200];
+        } catch (error) {
+          return [error, 404];
+        }
+      };
 }
