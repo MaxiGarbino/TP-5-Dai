c07c1122837120031b33a42ee249f576ec372561
Maximiliano Garbino <maximilianogarbino981@gmail.com>
2024-07-12 20:32:03 -0300

    2avance

--- src/controllers/location-controller.js
+++ src/controllers/location-controller.js
@@ -11,8 +11,8 @@ router.get('', async(req, res) => {
 });
 
 router.get('/:id', async (req, res) => {
-    const id = req.params.id;
-    const resArray = await svc.getByIdAsync(id);
+    let id = req.params.id;
+    let resArray = await svc.getByIdAsync(id);
     res.status(resArray[1]).send(resArray[0]);
     
 });
